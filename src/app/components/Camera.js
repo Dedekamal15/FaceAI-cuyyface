67c92d61123abfc0efce8139651285ef48faadde
TST <54031819+Dedekamal15@users.noreply.github.com>
2025-10-20 11:46:23 +0700

    Update Camera.js

--- src/app/components/Camera.js
+++ src/app/components/Camera.js
@@ -1,8 +1,59 @@
-import React from 'react'
+"use client"
+import React, {useRef, useState, useEffect, useMemo} from 'react'
+import Webcam from 'react-webcam'
 
+function usePotraitMode() {
+    const [isPotrait, setIsPotrait] = useState(false);
+    useEffect(() => {
+        const screenMedia = window.matchMedia("(orientation: portrait)");
+
+        const onChange = () => setIsPotrait(screenMedia.matches);
+        onChange()
+        screenMedia.addEventListener("change", onChange);
+        return () => screenMedia.removeEventListener?.("change", onChange);
+
+
+    }, [] )
+    return Potrait
+}
 function Camera() {
+    const webcamRef = useRef(null);
+    const resultRef = useRef(null);
+    const canvasRef = useRef(null);
+
+    const [photoDataUrl, setPhotoDataUrl] = useState("");
+    const [isLoading, setLoading] = useState(false);
+
+    const isPotrait = usePotraitMode();
+    const videoConstraints = useMemo(
+        () => ({
+            facingMode: "user",
+            Wwidth: {ideal: isPotrait ? 720 : 1280},
+            height: {ideal: isPotrait ? 1280: 720},
+            frameRata: {ideal: 30, max: 60}
+        }), []
+    )
+
+    function capturePhoto() {
+        const video = webcamRef.current?.video;
+        const canvas = canvasRef.current;
+
+    }    
   return (
-    <div className='y-[4px] h-[400px] border-2 p-2'>Camera</div>
+    <div>
+        <div className='relative w-full rounded-2xl overflow-hidden bg-black'>
+            <Webcam 
+                ref={webcamRef}
+                audio={false}
+                videoConstraints={videoConstraints}
+                mirrored={true}
+                className={'w-full ${isPotrait ?  "aspect-[16/9]" : "aspect-video}'}
+                screenshotFormat='image/jpeg'
+                screenshotQuality={0.9}
+            />
+
+        </div>   
+    </div>
   )
 }
 
